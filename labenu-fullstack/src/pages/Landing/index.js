4bcc6746db653361e8f63e277b03aaf95825699d
Renata Mitsue <rmkarato@gmail.com>
2020-09-23 20:08:14 -0300

    rotas landing, signup e login configuradas

---
+++ labenu-fullstack/src/pages/Landing/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+function Landing() {
+    let history = useHistory();
+    
+    return (
+        <div>
+            <h1>Landing</h1>
+        </div>
+    )
+}
+
+export default Landing;
---
+++ labenu-fullstack/src/pages/Signup/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+function Signup() {
+    let history = useHistory();
+    
+    return (
+        <div>
+            <h1>Signup</h1>
+        </div>
+    )
+}
+
+export default Signup;
