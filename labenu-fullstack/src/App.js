ec02a18b95ea1ce9e052350a918f2fe8aeff6032
Renata Mitsue <rmkarato@gmail.com>
2020-09-23 19:57:18 -0300

    configuração inicial pronta

---
+++ labenu-fullstack/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./assets/styles/global.css";
+
+function App() {
+  return (
+    <div>
+      <h1> Testando... </h1>
+    </div>
+  );
+}
+
+export default App;
